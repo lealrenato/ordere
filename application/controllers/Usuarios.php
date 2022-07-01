ce09aa92da228d909c7db22d79d2f9bdb79ffedd
lealrenato <renatolealhiss@gmail.com>
2022-06-30 21:45:48 -0300

    main

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') or exit('Ação não permitida');
+
+class Usuarios extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        //Definir se ha sessão
+    }
+
+    public function index()
+    {   
+
+        $data = array(
+
+            'titulo' => 'Usuários',
+            
+
+            'styles' => array(
+                'vendor/datatables/dataTables.bootstrap4.min.css'
+            ),
+            
+            'scripts' => array(
+                'vendor/datatables/jquery.dataTables.min.js',
+                'vendor/datatables/dataTables.bootstrap4.min.js',
+                'vendor/datatables/app.js',
+            ),  
+            'usuarios' => $this->ion_auth->users()->result(),
+        
+        );
+
+        //echo '<pre>';
+        //print_r($data['usuarios']);
+        //exit();
+
+        $this->load->view('layout/header', $data);
+        $this->load->view('usuarios/index');
+        $this->load->view('layout/footer');
+    }
+}
---
+++ application/models/Core_model.php
@@ -0,0 +1,97 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Core_model extends CI_Model {
+
+	//função para retornar todos os dados da tabela inicio 
+	public function get_all($tabela = NULL,$condicao = NULL)
+	{
+		if ($tabela) {
+            if (is_array($condicao)) {
+               $this->db->where($condicao);
+            }
+            return $this->db->get($tabela)->result();
+        }else {
+            return FALSE;
+        }
+	}
+    //função para retornar todos os dados da tabela inicio 
+
+    public function get_by_id($tabela = NULL,$condicao = NULL)
+	{
+		if ($tabela && is_array($condicao)) {
+            $this->db->where($condicao);
+            $this->db->limit(1);
+
+            return $this->db->get($tabela)->row();
+        }else {
+            return FALSE;
+        }
+	}
+
+
+    public function insert($tabela = NULL,$data = NULL, $get_last_id = NULL)
+	{
+		if ($tabela && is_array($data)) {
+             $this->db->insert($tabela,$data);
+             if ($get_last_id) {
+                $this->session->set_userdata('last_id',$this->db->insert_id());
+             }
+
+
+             if ($this->db->affected_rows()>0) {
+                $this->session->set_flashdata('sucesso','Dados salvos com sucesso');
+             } else {
+                $this->session->set_flashdata('erro','Erro ao salvar dados');
+             }
+             
+        }else {
+            return FALSE;
+        }
+	}
+
+
+    public function update($tabela = NULL,$data = NULL, $condicao = NULL)
+	{
+		if ($tabela && is_array($data) && is_array($condicao)) {
+            
+            if ($this->db->update($tabela,$data, $condicao)) {
+                $this->session->set_flashdata('sucesso','Dados salvos com sucesso');
+            } else {
+                $this->session->set_flashdata('erro','Erro ao atualisar os dados');
+            }
+                       
+        }else {
+            return FALSE;
+        }
+	}
+
+
+    public function delete($tabela = NULL,$condicao = NULL)
+	{
+
+        $this->db->db_debug = FALSE;
+		if ($tabela && is_array($condicao)) {
+            
+            $status = $this->db->delete($tabela,$condicao);
+            $error =  $this->db->error();
+
+            if (!$status) {
+               foreach($error as $code){
+                if ($code == 1451) {
+                    $this->session->set_flashdata('erro','Esse registro não podera ser excluido, pois esta sendo utilizado por outra tabela');
+                }
+               }
+            }else{
+                $this->session->set_flashdata('sucesso','Registro esluido com sucesso');
+            }
+
+            $this->db->db_debug = TRUE;
+                       
+        }else {
+            return FALSE;
+        }
+	}
+
+	
+}
